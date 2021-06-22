709e155696e1fbce1c61c01f4e9aa1d111366ed4
Mehdi Najafi <dev.mehdinajafi@gmail.com>
2021-06-22 12:33:26 +0430

    #1 convert MainNavbar to class component

--- dist/components/Navbar/MainNavbar/MainNavbar.tsx
+++ dist/components/Navbar/MainNavbar/MainNavbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './MainNavbar.module.scss';
 import Image from 'next/image';
 import { Button, Container } from 'react-bootstrap';
@@ -9,101 +9,121 @@ import License from './License/License';
 import Support from './Support/Support';
 import Link from 'next/link';
 
-const MainNavbar = () => {
-  const [isMenuOpen, setMenuOpen] = useState<boolean>(false);
-  const [dropDownOpend, setDropDownOpend] = useState<boolean>(false);
+export interface MainNavbarProps {}
 
-  const changeShowDropDown = () => {
-    setDropDownOpend((prevState) => !prevState);
-  };
+export interface MainNavbarState {
+  isMenuOpen: boolean;
+  dropDownOpend: boolean;
+}
 
-  const changeShowMenu = () => {
-    setMenuOpen((prevState) => {
+class MainNavbar extends React.Component<MainNavbarProps, MainNavbarState> {
+  constructor(props: MainNavbarProps) {
+    super(props);
+    this.state = { isMenuOpen: false, dropDownOpend: false };
+    this.changeShowDropDown = this.changeShowDropDown.bind(this);
+    this.changeShowMenu = this.changeShowMenu.bind(this);
+    this.closeSpace = this.closeSpace.bind(this);
+  }
+
+  changeShowDropDown() {
+    this.setState((prev) => {
+      return {
+        dropDownOpend: !prev.dropDownOpend,
+      };
+    });
+  }
+
+  changeShowMenu() {
+    this.setState((prevState) => {
       if (window.innerWidth <= 768) {
-        if (!prevState) {
+        if (!prevState.isMenuOpen) {
           document.querySelector('body').style.overflow = 'hidden';
         } else {
           document.querySelector('body').style.overflow = '';
         }
       }
-      return !prevState;
+      return {
+        isMenuOpen: !prevState.isMenuOpen,
+      };
     });
-  };
+  }
 
-  const closeSpace = () => {
-    if (!dropDownOpend) {
-      changeShowMenu();
+  closeSpace() {
+    if (!this.state.dropDownOpend) {
+      this.changeShowMenu();
     } else {
-      changeShowDropDown();
+      this.changeShowDropDown();
     }
-  };
+  }
 
-  return (
-    <div className={styles.mainNavbar} id="mainNavbar">
-      <Container fluid="md">
-        <div className="d-flex align-items-center justify-content-between">
-          <div className="d-flex align-items-center">
-            <a href="/">
-              <Image src="/logo-header.png" width={171} height={35} />
-            </a>
-            <div
-              className={styles.navItemWrapper}
-              style={{ right: isMenuOpen ? 0 : '-100%' }}
-            >
-              <Hosting changeShowDropDown={changeShowDropDown} />
-              <Domain changeShowDropDown={changeShowDropDown} />
-              <Server changeShowDropDown={changeShowDropDown} />
-              <License changeShowDropDown={changeShowDropDown} />
-              <Support changeShowDropDown={changeShowDropDown} />
-              <Link href="/fa/contact">
-                <a className={styles.jobsLinkOnNav}>
-                  <i className="fas fa-briefcase"></i>
-                  فرصت‌های شغلی
-                </a>
-              </Link>
-              <Link href="/fa/contact">
-                <a className={styles.contactUsLink}>
-                  <i className="fas fa-phone-alt"></i>
-                  تماس با ما
-                </a>
-              </Link>
+  render() {
+    return (
+      <div className={styles.mainNavbar} id="mainNavbar">
+        <Container fluid="md">
+          <div className="d-flex align-items-center justify-content-between">
+            <div className="d-flex align-items-center">
+              <a href="/">
+                <Image src="/logo-header.png" width={171} height={35} />
+              </a>
+              <div
+                className={styles.navItemWrapper}
+                style={{ right: this.state.isMenuOpen ? 0 : '-100%' }}
+              >
+                <Hosting changeShowDropDown={this.changeShowDropDown} />
+                <Domain changeShowDropDown={this.changeShowDropDown} />
+                <Server changeShowDropDown={this.changeShowDropDown} />
+                <License changeShowDropDown={this.changeShowDropDown} />
+                <Support changeShowDropDown={this.changeShowDropDown} />
+                <Link href="/fa/contact">
+                  <a className={styles.jobsLinkOnNav}>
+                    <i className="fas fa-briefcase"></i>
+                    فرصت‌های شغلی
+                  </a>
+                </Link>
+                <Link href="/fa/contact">
+                  <a className={styles.contactUsLink}>
+                    <i className="fas fa-phone-alt"></i>
+                    تماس با ما
+                  </a>
+                </Link>
+              </div>
+              <button
+                className={styles.closeSpace}
+                onClick={this.closeSpace}
+                style={{ left: this.state.isMenuOpen ? 0 : '-100%' }}
+              ></button>
             </div>
-            <button
-              className={styles.closeSpace}
-              onClick={closeSpace}
-              style={{ left: isMenuOpen ? 0 : '-100%' }}
-            ></button>
-          </div>
-          <Link href="/fa/jobs">
-            <a className={styles.jobTitlesBtn}>
-              <i className="fas fa-briefcase"></i>
-              <span>فرصت‌های شغلی</span>
-            </a>
-          </Link>
-          <Button
-            variant="light"
-            className={styles.hamburgerMenuBtn}
-            onClick={changeShowMenu}
-            data-close
-          >
-            {isMenuOpen ? (
-              <i className="fas fa-chevron-right"></i>
+            <Link href="/fa/jobs">
+              <a className={styles.jobTitlesBtn}>
+                <i className="fas fa-briefcase"></i>
+                <span>فرصت‌های شغلی</span>
+              </a>
+            </Link>
+            <Button
+              variant="light"
+              className={styles.hamburgerMenuBtn}
+              onClick={this.changeShowMenu}
+              data-close
+            >
+              {this.state.isMenuOpen ? (
+                <i className="fas fa-chevron-right"></i>
+              ) : (
+                <i className="fas fa-bars"></i>
+              )}
+            </Button>
+            {this.state.dropDownOpend ? (
+              <div
+                className={styles.closeSpaceForDropDown}
+                onClick={this.changeShowDropDown}
+              ></div>
             ) : (
-              <i className="fas fa-bars"></i>
+              <div className="d-none"></div>
             )}
-          </Button>
-          {dropDownOpend ? (
-            <div
-              className={styles.closeSpaceForDropDown}
-              onClick={changeShowDropDown}
-            ></div>
-          ) : (
-            <div className="d-none"></div>
-          )}
-        </div>
-      </Container>
-    </div>
-  );
-};
+          </div>
+        </Container>
+      </div>
+    );
+  }
+}
 
 export default MainNavbar;

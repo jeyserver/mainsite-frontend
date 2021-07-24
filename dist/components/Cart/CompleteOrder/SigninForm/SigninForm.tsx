26b4c83537b892679279b39c8ecf01d1f72daf8a
Mehdi Najafi <dev.mehdinajafi@gmail.com>
2021-07-24 10:13:27 +0430

    Fix #23 forms and errors

--- dist/components/Cart/CompleteOrder/SigninForm/SigninForm.tsx
+++ dist/components/Cart/CompleteOrder/SigninForm/SigninForm.tsx
@@ -1,13 +1,28 @@
+import axios from 'axios';
 import classNames from 'classnames';
 import * as React from 'react';
-import { InputGroup, Row, Col, Form } from 'react-bootstrap';
+import { InputGroup, Row, Col, Form, Spinner } from 'react-bootstrap';
 import { countriesType } from '../../../../pages/order/cart/complete';
 import { ReactPhonenumber } from '../../../ReactPhonenumber/ReactPhonenumber';
+import { NotificationManager } from 'react-notifications';
 import styles from './SigninForm.module.scss';
+import { connect } from 'react-redux';
+import { login } from '../../../../redux/actions';
+
+type error = 'data_validation' | 'data_duplicate';
+
+const showError = (errorMsg: error) => {
+  if (errorMsg === 'data_duplicate') {
+    return 'داده وارد شده تکراری است';
+  } else if (errorMsg === 'data_validation') {
+    return 'داده وارد شده معتبر نیست';
+  }
+};
 
 export interface SigninFormProps {
   countries: countriesType;
   defaultCountrySelected: string;
+  login: (loginDetails: any) => void;
 }
 
 export interface SigninFormState {
@@ -15,6 +30,9 @@ export interface SigninFormState {
   isSelectHide: boolean;
   emailPhonenumberInput: string;
   password: string;
+  emailPhoneNumberError: error;
+  passwordError: error;
+  submitBtnLoading: boolean;
 }
 
 class SigninForm extends React.Component<SigninFormProps, SigninFormState> {
@@ -25,6 +43,9 @@ class SigninForm extends React.Component<SigninFormProps, SigninFormState> {
       isSelectHide: true,
       emailPhonenumberInput: '',
       password: '',
+      emailPhoneNumberError: 'data_validation',
+      passwordError: 'data_validation',
+      submitBtnLoading: false,
     };
     this.signin = this.signin.bind(this);
     this.changePhoneNumber = this.changePhoneNumber.bind(this);
@@ -38,15 +59,46 @@ class SigninForm extends React.Component<SigninFormProps, SigninFormState> {
     if (form.checkValidity() === false) {
       e.stopPropagation();
     } else {
-      //go
+      this.setState({ submitBtnLoading: true });
+
+      axios
+        .get(
+          `https://jsonblob.com/api/jsonBlob/57cf67c8-eb0b-11eb-8813-cda002dae790`
+        )
+        .then((respone) => {
+          this.setState({ submitBtnLoading: false });
+
+          // this.props.loginDetails()
+
+          // if (respone.data.status) {
+          //   this.setState({ submitBtnLoading: false });
+          //   // this.props.router.push('/order/cart/review');
+          // } else if (!respone.data.status) {
+          //   respone.data.error.forEach((errorItem) => {
+          //     if (errorItem.input === 'credential') {
+          //       form.credential.value = '';
+          //       this.setState({ emailPhoneNumberError: errorItem.code });
+          //     } else if (errorItem.input === 'password') {
+          //       form.credential.value = '';
+          //       this.setState({ passwordError: errorItem.code });
+          //     }
+          //   });
+          //   this.setState({ submitBtnLoading: false });
+          // }
+        })
+        .catch((error) => {
+          this.setState({ submitBtnLoading: false });
+          NotificationManager.error(
+            'ارتباط با سامانه بدرستی انجام نشد، لطفا مجددا تلاش کنید.',
+            'خطا'
+          );
+        });
     }
 
     this.setState({ signinFormValidated: true });
   }
 
   changePhoneNumber(phoneNumber, phonenumberObj, value, isFocusedOnInput) {
-    console.log(isFocusedOnInput);
-
     if (/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/g.test(value)) {
       this.setState({ isSelectHide: false });
     } else {
@@ -82,6 +134,7 @@ class SigninForm extends React.Component<SigninFormProps, SigninFormState> {
                 placeholder: 'ایمیل یا شماره همراه',
                 icon: 'fa fa-user',
               }}
+              errorCode={this.state.emailPhoneNumberError}
             />
           </div>
         </InputGroup>
@@ -103,14 +156,25 @@ class SigninForm extends React.Component<SigninFormProps, SigninFormState> {
             کلمه عبور
           </span>
           <Form.Control.Feedback type="invalid">
-            داده وارد شده معتبر نیست
+            {showError(this.state.passwordError)}
           </Form.Control.Feedback>
         </InputGroup>
 
         <Row className="justify-content-center">
           <Col md={6}>
-            <button type="submit" className={styles.loginbtn}>
-              ورود
+            <button
+              type="submit"
+              className={styles.loginbtn}
+              disabled={this.state.submitBtnLoading}
+            >
+              {this.state.submitBtnLoading ? (
+                <>
+                  <Spinner animation="border" size="sm" />
+                  <span>لطفا صبر کنید</span>
+                </>
+              ) : (
+                'ورود'
+              )}
             </button>
           </Col>
         </Row>
@@ -119,4 +183,4 @@ class SigninForm extends React.Component<SigninFormProps, SigninFormState> {
   }
 }
 
-export default SigninForm;
+export default connect(null, { login })(SigninForm);

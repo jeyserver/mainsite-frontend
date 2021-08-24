1023a6fb128aa20ef0f978b79cc70177bf84f4ae
Mehdi Najafi <dev.mehdinajafi@gmail.com>
2021-08-24 16:04:02 +0430

    Fix #28 and add local flags

---
+++ dist/helper/types/products/VPS/plan.ts
@@ -0,0 +1,23 @@
+export interface IVPSPlan {
+  id: number;
+  title: string;
+  country: {
+    code: string;
+    name: string;
+  };
+  price: number;
+  penalty_ratio: number;
+  virtualer: number;
+  hard: number;
+  hardtype: number;
+  cpu: number;
+  bandwidth: number | null;
+  ram: number;
+  setup: number;
+  addonip: number;
+  currency: number;
+  status: number;
+  sold_out: boolean;
+  periods: number[];
+  is_available: boolean;
+}

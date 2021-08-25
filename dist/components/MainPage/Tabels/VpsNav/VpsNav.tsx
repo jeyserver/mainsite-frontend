8bd67049020080ac431ea7fe4725c2552f4e1b62
Mehdi Najafi <dev.mehdinajafi@gmail.com>
2021-08-25 16:07:42 +0430

    #28 Use lib folder information

--- dist/components/MainPage/Tabels/VpsNav/VpsNav.tsx
+++ dist/components/MainPage/Tabels/VpsNav/VpsNav.tsx
@@ -2,43 +2,7 @@ import * as React from 'react';
 import { Row, Col, Dropdown } from 'react-bootstrap';
 import Link from 'next/link';
 import styles from './VpsNav.module.scss';
-
-const vpsNavData = {
-  professional: [
-    {
-      name_en: 'france',
-      name_fa: 'فرانسه',
-    },
-    {
-      name_en: 'germany',
-      name_fa: 'آلمان',
-    },
-    {
-      name_en: 'iran',
-      name_fa: 'ایران',
-    },
-  ],
-  economic: [
-    {
-      name_en: 'france',
-      name_fa: 'فرانسه',
-    },
-    {
-      name_en: 'germany',
-      name_fa: 'آلمان',
-    },
-    {
-      name_en: 'iran',
-      name_fa: 'ایران',
-    },
-  ],
-  storage: [
-    {
-      name_en: 'france',
-      name_fa: 'فرانسه',
-    },
-  ],
-};
+import vpsesWithCountries from '../../../../lib/products/vps';
 
 const tarnslateType = (type) => {
   switch (type) {
@@ -59,8 +23,8 @@ class VpsNav extends React.Component {
       <Row className="justify-content-center">
         <Col md={10}>
           <ul className={styles.vpsNav}>
-            {Object.keys(vpsNavData).map((type) => {
-              return vpsNavData[type].map((country) => (
+            {Object.keys(vpsesWithCountries).map((type) => {
+              return vpsesWithCountries[type].map((country) => (
                 <li key={country.name_en}>
                   <Link
                     href={`/server/vps/${type}#server_vps_${type}_${country.name_en}`}
@@ -82,7 +46,7 @@ class VpsNav extends React.Component {
                 </Dropdown.Toggle>
 
                 <Dropdown.Menu className={styles.dropdownMenu}>
-                  {vpsNavData.professional.map((country) => (
+                  {vpsesWithCountries.professional.map((country) => (
                     <Link
                       key={country.name_en}
                       href={`/server/vps/professional#server_vps_professional_${country.name_en}`}
@@ -101,7 +65,7 @@ class VpsNav extends React.Component {
                 </Dropdown.Toggle>
 
                 <Dropdown.Menu className={styles.dropdownMenu}>
-                  {vpsNavData.economic.map((country) => (
+                  {vpsesWithCountries.economic.map((country) => (
                     <Link
                       key={country.name_en}
                       href={`/server/vps/professional#server_vps_professional_${country.name_en}`}
@@ -113,7 +77,7 @@ class VpsNav extends React.Component {
               </Dropdown>
             </li>
 
-            {vpsNavData.storage.map((country) => (
+            {vpsesWithCountries.storage.map((country) => (
               <li key={country.name_fa}>
                 <Link
                   href={`/server/vps/professional#server_vps_storage_${country.name_en}`}
---
+++ dist/lib/products/vps/index.ts
@@ -0,0 +1,38 @@
+const vpsesWithCountries = {
+  professional: [
+    {
+      name_en: 'france',
+      name_fa: 'فرانسه',
+    },
+    {
+      name_en: 'germany',
+      name_fa: 'آلمان',
+    },
+    {
+      name_en: 'iran',
+      name_fa: 'ایران',
+    },
+  ],
+  economic: [
+    {
+      name_en: 'france',
+      name_fa: 'فرانسه',
+    },
+    {
+      name_en: 'germany',
+      name_fa: 'آلمان',
+    },
+    {
+      name_en: 'iran',
+      name_fa: 'ایران',
+    },
+  ],
+  storage: [
+    {
+      name_en: 'france',
+      name_fa: 'فرانسه',
+    },
+  ],
+};
+
+export default vpsesWithCountries;

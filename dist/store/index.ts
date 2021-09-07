7665799f22e848bca86259b78ebdc0ea5e6d1f41
Mehdi Najafi <dev.mehdinajafi@gmail.com>
2021-09-07 22:05:30 +0430

    #36 Save theme in local storage

--- dist/store/index.ts
+++ dist/store/index.ts
@@ -1,17 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit';
-import currenciesReducer from './Currencies';
-import authReducer from './Auth';
-import languageReducer from './Language';
-import themeReducer from './Theme';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import currencies from './Currencies';
+import auth from './Auth';
+import language from './Language';
+import theme from './Theme';
 
 export const store = configureStore({
-  reducer: {
-    currencies: currenciesReducer,
-    auth: authReducer,
-    language: languageReducer,
-    theme: themeReducer,
-  },
+  reducer: persistReducer(
+    { key: 'root', version: 1, storage },
+    combineReducers({
+      currencies,
+      auth,
+      language,
+      theme,
+    })
+  ),
 });
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
